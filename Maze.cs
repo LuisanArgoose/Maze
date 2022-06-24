e11014e647caa79419aa4aa729b5b10f6e784ded
LuisanToxic <69768825+LuisanToxic@users.noreply.github.com>
2022-06-24 23:47:53 +0300

    Add files via upload

--- Maze.cs
+++ Maze.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace MazeLib
 {
     /// <summary>
@@ -95,7 +95,7 @@ namespace MazeLib
             return (Map);
         }
         /// <summary>
-        /// Метод создание матрицы лабиринта по уже заданным рамерам
+        /// Метод создание матрицы лабиринта по уже заданным рамерам 
         /// </summary>
         /// <returns>Возвращает матрицу лабиринта по уже заданным размерам</returns>
         public int[,] GetNewLevel()
@@ -103,6 +103,7 @@ namespace MazeLib
             Generation();
             return (Map);
         }
+
         private void Generation()
         {
             Map = new int[MapSizeXY[1], MapSizeXY[0]];  
@@ -115,13 +116,15 @@ namespace MazeLib
             }
             int ArrowX = rnd.Next(1, Map.GetLength(1) - 1); // Случайный X
             int ArrowY = rnd.Next(1, Map.GetLength(0) - 1); // Случайный Y
-            Map[ArrowY, ArrowX] = 2; // Создание стрелки в случайном месте
-            MaxWay = 0;
-            Worm(ArrowX, ArrowY, 0, MaxWay);
+             
+            //MaxWay = 0;
+            //Worm(ArrowX, ArrowY, 0, MaxWay);
+            WormV2(ArrowX, ArrowY);
+            Map[ArrowY, ArrowX] = 2;// Создание стрелки в случайном месте
             Map[EndPointY, EndPointX] = 3;
 
         }
-        private void Worm(int x, int y, int BackDirection, int MaxWaylocal)
+        private void Worm(int x, int y, int BackDirection, int MaxWaylocal) //Метод с рекурсией
         {
             MaxWaylocal++; //Иттератор для максимального пути
             if(MaxWaylocal > MaxWay)
@@ -191,6 +194,145 @@ namespace MazeLib
                         continue;
                     }
                 }
+            }
+        }
+        private void WormV2(int x, int y, int BackDirection = 0, int MaxWaylocal = 0) // Новый метод, который использует рекурсию только для веток
+        {
+            List<TypeObject> NewWays = new List<TypeObject>();
+            int[] Sides = { 0, 0, 0 };
+            bool NewWay = false;
+            //MaxWaylocal++; //Иттератор для максимального пути
+
+            bool debug; //Результат проверки доступности клетки
+                        //int SideCount = 10; //Максимальное количество сторон для проверки
+            int Count;
+            while (true)
+            {
+                if (MaxWaylocal > MaxWay)
+                {
+                    MaxWay = MaxWaylocal;
+                    EndPointX = x;
+                    EndPointY = y;
+                }
+                Count = 0;
+                Sides = new int[] { 0, 0, 0 };
+                NewWay = false;
+                for (int i = 0; i < 3; i++) //Перебор каждой стороны
+                {
+                    int DirectionSide = rnd.Next(1, 5);
+                    while (DirectionSide == BackDirection || DirectionSide == Sides[0] || DirectionSide == Sides[1] || DirectionSide == Sides[2]) //Выбирает не сторону, откуда пришел
+                    {
+                        DirectionSide = rnd.Next(1, 5);
+                    }
+                    Sides[i] = DirectionSide;
+                    if (DirectionSide == 1) //Вправо
+                    {
+                        debug = BoxCheck(x + 1, y, DirectionSide); // true Если клетки вокруг позволяют проделать разрез
+                        if (debug)
+                        {
+                            if (NewWay)
+                            {
+                                NewWays.Add(new TypeObject(x, y, 3, MaxWaylocal));
+                            }
+                            else
+                            {
+                                x++;
+                                Map[y, x] = 0;
+                                BackDirection = 3; // 3 - это сторона, с которой прийдет сигнал
+                                MaxWaylocal++;
+                                NewWay = true;
+                            }
+                        }
+                        else
+                        {
+                            Count++;
+
+                        }
+                    }
+                    else if (DirectionSide == 2) //Вверх
+                    {
+                        debug = BoxCheck(x, y - 1, DirectionSide); // true Если клетки вокруг позволяют проделать разрез
+                        if (debug)
+                        {
+                            if (NewWay)
+                            {
+                                NewWays.Add(new TypeObject(x, y, 4, MaxWaylocal));
+                            }
+                            else
+                            {
+                                y--;
+                                Map[y, x] = 0;
+                                BackDirection = 4;// 4 - это сторона, с которой прийдет сигнал
+                                MaxWaylocal++;
+                                NewWay = true;
+                            }
+
+                        }
+                        else
+                        {
+                            Count++;
+                        }
+                    }
+                    else if (DirectionSide == 3) //Влево
+                    {
+                        debug = BoxCheck(x - 1, y, DirectionSide); // true Если клетки вокруг позволяют проделать разрез
+                        if (debug)
+                        {
+                            if (NewWay)
+                            {
+                                NewWays.Add(new TypeObject(x, y, 1, MaxWaylocal));
+                            }
+                            else
+                            {
+                                x--;
+                                Map[y, x] = 0;
+                                BackDirection = 1;// 1 - это сторона, с которой прийдет сигнал
+                                MaxWaylocal++;
+                                NewWay = true;
+                            }
+                        }
+                        else
+                        {
+                            Count++;
+
+                        }
+                    }
+                    else if (DirectionSide == 4) //Вниз
+                    {
+                        debug = BoxCheck(x, y + 1, DirectionSide); // true Если клетки вокруг позволяют проделать разрез
+                        if (debug)
+                        {
+                            if (NewWay)
+                            {
+                                NewWays.Add(new TypeObject(x, y, 2, MaxWaylocal));
+                            }
+                            else
+                            {
+                                y++;
+                                Map[y, x] = 0;
+                                BackDirection = 2;// 2 - это сторона, с которой прийдет сигнал
+                                MaxWaylocal++;
+                                NewWay = true;
+                            }
+                        }
+                        else
+                        {
+                            Count++;
+
+                        }
+                    }
+
+
+                }
+                if (Count == 3)
+                {
+                    foreach (TypeObject Way in NewWays)
+                    {
+                        WormV2(Way.x, Way.y, Way.BackDirection);
+                    }
+                    break;
+                }
+
             }
         }
         private bool BoxCheck(int x, int y, int DirectionSide)
@@ -277,7 +419,22 @@ namespace MazeLib
             }
             return false;
         }
-    }   
+    }
+    public class TypeObject
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public int BackDirection { get; set; }
+        public int MaxWaylocal { get; set; }
+        public TypeObject(int x, int y, int BackDirection, int maxWaylocal)
+        {
+            this.x = x;
+            this.y = y;
+            this.BackDirection = BackDirection;
+            this.MaxWaylocal = maxWaylocal;
+        }
+
+    }
 }
 
 
